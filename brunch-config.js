acc5d4164a95fe8730e0cc947c36e83a77d3eec1
Paul Miller <paul@paulmillr.com>
2016-08-31 23:34:02 +0300

    Update brunch-config.js

--- brunch-config.js
+++ brunch-config.js
@@ -1,8 +1,6 @@
-module.exports = {
-  // See http://brunch.io for documentation.
-  files: {
-    javascripts: {joinTo: 'app.js'},
-    stylesheets: {joinTo: 'app.css'},
-    templates: {joinTo: 'app.js'}
-  }
-}
+// See http://brunch.io for documentation.
+exports.files = {
+  javascripts: {joinTo: 'app.js'},
+  stylesheets: {joinTo: 'app.css'},
+  templates: {joinTo: 'app.js'}
+};
