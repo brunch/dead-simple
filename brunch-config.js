f3b064f829c14a1fe58bc6d063688c978ec397d1
Paul Miller <paul@paulmillr.com>
2017-02-07 20:09:39 +0200

    Improve UX.

--- brunch-config.js
+++ brunch-config.js
@@ -1,6 +1,5 @@
 // See http://brunch.io for documentation.
 exports.files = {
   javascripts: {joinTo: 'app.js'},
-  stylesheets: {joinTo: 'app.css'},
-  templates: {joinTo: 'app.js'}
+  stylesheets: {joinTo: 'app.css'}
 };
